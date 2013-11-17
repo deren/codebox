630563219a4038e2ca3b26a15e0a816a970eb838
Deren Wu <deren.g@gmail.com>
2013-11-17 15:22:38 +0800

    Change prompt for development

--- codebox.sh
+++ codebox.sh
@@ -208,3 +208,48 @@ function godir () {
 	fi
 	cd $T/$pathname
 }
+
+
+function parse_git_branch {
+git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+function setPS1()
+{
+# Ref :
+#      http://bash.cyberciti.biz/guide/Changing_bash_prompt
+#      http://xta.github.io/HalloweenBash/
+local K="\[\033[0;30m\]"    # black
+local R="\[\033[0;31m\]"    # red
+local G="\[\033[0;32m\]"    # green
+local Y="\[\033[0;33m\]"    # yellow
+local B="\[\033[0;34m\]"    # blue
+local M="\[\033[0;35m\]"    # magenta
+local C="\[\033[0;36m\]"    # cyan
+local W="\[\033[0;37m\]"    # white
+
+# emphasized (bolded) colors
+local EMK="\[\033[1;30m\]"
+local EMR="\[\033[1;31m\]"
+local EMG="\[\033[1;32m\]"
+local EMY="\[\033[1;33m\]"
+local EMB="\[\033[1;34m\]"
+local EMM="\[\033[1;35m\]"
+local EMC="\[\033[1;36m\]"
+local EMW="\[\033[1;37m\]"
+
+# background colors
+local BGK="\[\033[40m\]"
+local BGR="\[\033[41m\]"
+local BGG="\[\033[42m\]"
+local BGY="\[\033[43m\]"
+local BGB="\[\033[44m\]"
+local BGM="\[\033[45m\]"
+local BGC="\[\033[46m\]"
+local BGW="\[\033[47m\]"
+
+local NONE="\[\033[0m\]"    # unsets color to term's fg color
+echo "${BGK}${C}\u@\h: ${G}\W${EMB}\$(parse_git_branch)${NONE}\\$ "
+}
+
+# Remove this line if you do not want to change prompt
+export PS1=$(setPS1)
